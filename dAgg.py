c1ec765e283e075eeba33b0258417cd2fe209cfc
Robert <rrochlin@yahoo.com>
2020-12-24 23:33:59 -0800

    notebook updated

--- dAgg.py
+++ dAgg.py
@@ -12,4 +12,13 @@ loops=len(all_csv_files)
 
 data = {}
 for idx,x in enumerate(all_csv_files):
-    data[x[7:len(x)-4]] = pd.read_csv(x,header=1)
+    df = pd.read_csv(x,header=1,parse_dates=[[0,1]])
+    df.columns = df.columns.str.replace(' ', '') 
+    try:
+        df.drop(df[df['Date_Time'] < pd.Timestamp('12/22/2020')].index, inplace = True)
+    except TypeError:
+        df.drop(df[df['Date_Time'] == '     0/0/0      0:0:0'].index, inplace = True)
+        print(x)
+        df['Date_Time'] = pd.to_datetime(df['Date_Time'])
+        df.drop(df[df['Date_Time'] < pd.Timestamp('12/22/2020')].index, inplace = True)
+    data[x[7:len(x)-4]] = df.reset_index(drop=True)
